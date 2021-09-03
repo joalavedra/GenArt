af76ff1f68faaeeeb1fecbadec12a610a4461b0d
david mas <david.mas.p@gmail.com>
2021-09-03 18:19:56 +0200

    :construction: experimental
    Changed the squares completely, ara son escates

--- vasarely/sketch.js
+++ vasarely/sketch.js
@@ -27,13 +27,14 @@ class Cell {
     this.hp = hp;
     this.vp = vp;
     this.width = width;
-    this.circwidth = 0.84 * width;
+    //this.circwidth = 0.84 * width;
+    this.circwidth = random(1.2,2.5) *  width;
   }
   draw(){
     fill(this.colorSquare);
-    rect(this.hp, this.vp, this.width, this.width);
+    //rect(this.hp, this.vp, this.width, this.width);
     fill(this.colorCircle);
-    circle(this.hp, this.vp,this.circwidth,this.circwidth);
+    circle(this.hp, this.vp,this.circwidth);
   }
 }
 
@@ -43,10 +44,10 @@ class Grid {
               columns, 
               totalWidth, 
               // totalHeight, 
-              colorNumMin = 183, // this is orange
-              colorNumMax = 280, // this is blue
+              HueMin = 70, // this is orange
+              HueMax = 150, // this is blue
               limitLeft = 50, // min 50 
-              limitRight = 95) { //max 100
+              limitRight = 65) { //max 100
     // number of cells horizontally
     width = totalWidth/columns;
     // number of cells vertically
@@ -56,16 +57,23 @@ class Grid {
 
     const totalColorLength = limitRight - limitLeft;
     const colorStep = totalColorLength/columns;
+
+    let pN = 100;
     
     for (let i = 0; i < rows; i++) {
-      let colorNum = round(random(colorNumMin,colorNumMax));
-      console.log(colorNum);
+      pN = pN+100;
       for (let j = 0; j < columns; j++) {
+        const perlinValue = noise(pN+j);
+        console.log(perlinValue);
+        let Huec = round(map(perlinValue, 0, 1, HueMin, HueMax));
+        console.log(Huec);
+        let Hues = round(random(HueMin,HueMax));
+        //console.log(Hue);
         //console.log(`${i}`);
         const percentc = limitLeft + (j * colorStep);
         const percents = limitRight - (j * colorStep); 
-        const cc = color(`hsl(${colorNum}, 100%, ${percentc}%)`);
-        const cs = color(`hsl(${colorNum}, 100%, ${percents}%)`);
+        const cc = color(`hsl(${Huec}, 79%, ${percentc}%)`);
+        const cs = color(`hsl(${Hues}, 70%, ${percents}%)`);
         const hp = (j+0.5)*width;
         const vp = (i+0.5)*height;
         // create a new cell
